86ceb6c759b197ad8dd9dd33b0d40fa56ee937ac
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2023-10-24 16:13:48 -0500

    Update lola_kraken_hway_services.py

--- lolakrakenpy/lola_kraken_hway_services.py
+++ lolakrakenpy/lola_kraken_hway_services.py
@@ -10,7 +10,6 @@ class LolaHwayServicesManager:
     def validatePhone(self, phone):    
         url = f'{self.lola_kraken_url}/hway/is-phone-available/{phone}'
         headers = {'x-lola-auth': self.lola_token}
-        data = {'lead': self.lead}
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()['data']
