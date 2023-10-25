b25615e1f6235cdce9d479a8f7faf735ee01a256
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2023-10-25 10:20:19 -0500

    Update lola_kraken_hway_services.py

--- lolakrakenpy/lola_kraken_hway_services.py
+++ lolakrakenpy/lola_kraken_hway_services.py
@@ -8,11 +8,16 @@ class LolaHwayServicesManager:
         self.session = session
         
 
-    def validatePhone(self, phone):    
-        url = f'{self.lola_kraken_url}/hway/is-phone-available/{phone}'
-        headers = {'x-lola-auth': self.lola_token}
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        return response.json()
+    def validatePhone(self, phone):
+        try:
+            url = f'{self.lola_kraken_url}/hway/is-phone-available/{phone}'
+            headers = {'x-lola-auth': self.lola_token}
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except Exception as error:
+            print("error in service /hway/is-phone-available/" + error)
+            return Exception(error)
+            
     
    
