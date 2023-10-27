1492759cee65b89256004d1a9e1906c60631fa79
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2023-10-27 11:34:25 -0500

    Update Lola_Kraken_vision_services.py

--- lolakrakenpy/Lola_Kraken_vision_services.py
+++ lolakrakenpy/Lola_Kraken_vision_services.py
@@ -41,7 +41,7 @@ class LolaVisionServicesManager:
                 'url': url,
                 'imgBase64': image
             }   
-            data = LolaKrakenUtils.deleteKeyundefined(data);
+            data = LolaKrakenUtils.deleteKeyundefined(data)
             print(data)
             response = requests.post(endpoint, headers=headers, json=data)
             response.raise_for_status()
