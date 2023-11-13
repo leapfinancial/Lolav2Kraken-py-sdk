e2663549f494e041dcac8b070c816b27f1610dc9
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2023-11-13 17:24:21 -0500

    Update Lola_Kraken_vision_services.py

--- lolakrakenpy/Lola_Kraken_vision_services.py
+++ lolakrakenpy/Lola_Kraken_vision_services.py
@@ -61,6 +61,48 @@ class LolaVisionServicesManager:
             print(e)
             raise ValueError(e)
         
+    def scanGenericDocument(self, url=None, image=None):
+        """
+        Scans a generic ID from an image or URL.
+
+        Args:
+            url (str): The URL of the image.
+            image (bytes): The image data.
+
+        Raises:
+            ValueError: If neither url nor image is provided.
+
+        Returns:
+            dict: The response JSON.
+        """
+        
+        try:
+            if url is None and image is None:
+                raise ValueError('Either url or image must be provided')
+
+            endpoint = f'{self.lola_kraken_url}/vision/process/document/generic'
+            headers = {'x-lola-auth': self.lola_token, 'Content-Type': 'application/json'}
+            data ={
+                'url': url,
+                'imgBase64': image
+            }   
+            
+            """""
+            data ={
+                'url': url,
+                'imgBase64': image
+            }   
+            data = LolaKrakenUtils.deleteKeyUndefined(data)
+            """""
+            data = OcrGenericSchema(**data).model_dump(exclude_none=True)
+            print(data)
+            response = requests.post(endpoint, headers=headers, json=data)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            print(e)
+            raise ValueError(e)
+        
     def extractFace(self, url=None, image=None):
         """
         Extract face from an image or URL.
