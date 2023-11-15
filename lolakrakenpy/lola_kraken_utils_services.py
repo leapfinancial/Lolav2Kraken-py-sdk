1fa940a7acde1ff32ad4c7cb6e27b33465efb1d7
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2023-11-15 13:59:25 -0500

    Update lola_kraken_utils_services.py

--- lolakrakenpy/lola_kraken_utils_services.py
+++ lolakrakenpy/lola_kraken_utils_services.py
@@ -16,6 +16,7 @@ class LolaUtilsServicesManager:
         """
         try:
             session = self.session
+            print(session)
             chatlead = session['lead']
             sessionStore = sessionStore
             
