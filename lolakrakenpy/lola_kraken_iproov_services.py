aaa96638893aad9967a3a057598754400df2e180
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2024-01-22 11:25:29 -0500

    Update lola_kraken_iproov_services.py

--- lolakrakenpy/lola_kraken_iproov_services.py
+++ lolakrakenpy/lola_kraken_iproov_services.py
@@ -50,7 +50,7 @@ class LolaIproovServicesManager:
         
         except Exception as e:
             raise ValueError(e)
-    def claimLink(self,returnUrl:str,theme:None,sessionStore=None,develoment:bool=False,assuranceType = 'genuine_presence'):
+    def claimLink(self,returnUrl:str,theme:None,sessionStore=None,develoment:bool=False,assuranceType = 'liveness'):
         """
         Claims a Link
         
