0ba07cab962ce3c65fd79d6f43d1f54c16b6c9ae
Andres Pinto <andres.pinto2203@gmail.com>
2024-01-25 18:07:14 -0400

    added util service

--- lolakrakenpy/shemas/face_functions_schema.py
+++ lolakrakenpy/shemas/face_functions_schema.py
@@ -1,20 +1,22 @@
 from pydantic import BaseModel
 
+
 class FaceCropSchema(BaseModel):
-    tracerId: str | None =None
-    session: object | None =None
-    image: object | None =None
-    image_b64: str | None =None
-    image_url: str | None =None
-    enableEncodings: bool | None=None   
+    tracerId: str | None = None
+    session: object | None = None
+    image: object | None = None
+    image_b64: str | None = None
+    image_url: str | None = None
+    enableEncodings: bool | None = None
+
 
 class FaceMatchSchema(BaseModel):
-    tracerId: str | None =None
-    session: object | None =None
-    image1: object | None =None
-    image1_b64: str | None =None
-    image1_url: str | None =None
-    image2: object | None =None
-    image2_b64: str | None =None
-    image2_url: str | None =None
-    enableEncodings: bool | None =None
+    tracerId: str | None = None
+    session: object | None = None
+    image1: object | None = None
+    image1_b64: str | None = None
+    image1_url: str | None = None
+    image2: object | None = None
+    image2_b64: str | None = None
+    image2_url: str | None = None
+    enableEncodings: bool | None = None
