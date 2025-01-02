be3191556fe5e97f7bbb0f18f4695e1954d91b60
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2025-01-02 09:56:56 -0500

    Update iproov_shema.py

--- lolakrakenpy/shemas/iproov_shema.py
+++ lolakrakenpy/shemas/iproov_shema.py
@@ -7,6 +7,14 @@ class metadataSchema(BaseModel):
     assuranceType: str | None
     lolaURL: str | None
     theme: object | None
+    
+class metadataSchemaCallback(BaseModel):
+    returnURL: str | None
+    operation: str | None
+    assuranceType: str | None
+    lolaURL: str | None
+    theme: object | None
+    callbackURL: str | None
 
 class claimTokenSchema(BaseModel):
     baseUrl: str | None
@@ -18,4 +26,4 @@ class claimTokenSchemaCallback(BaseModel):
     baseUrl: str | None
     chatLead: object | None
     sessionStore: object  | None
-    metadata: metadataSchema | None
+    metadata: metadataSchemaCallback | None
